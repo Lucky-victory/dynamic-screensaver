2f25b434361c38b608cf8c0fbd4b897665147cfe
lucky-victory <blizwonder25@gmail.com>
2021-11-30 01:29:27 +0100

    moved files to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path=require('path');
 module.exports = {
-  entry:'./main.js',
+  entry:'./dist/main.js',
   output: {
-    path:path.join(__dirname+'/dist'),
+    path:path.resolve(__dirname,'dist'),
     filename: 'bundle.js',
   },
 };
